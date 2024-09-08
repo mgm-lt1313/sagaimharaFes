d6023d76e02750f5bda612ffd33cf4c79e73f0fb
mgm-lt133 <a8122135@gsuite.si.aoyama.ac.jp>
2024-09-08 20:20:42 +0900

    'firstCommit'

---
+++ surprised/sketch.js
@@ -0,0 +1,133 @@
+let fireworks = [];
+let gravity;
+let fireworkImage; // 花火の玉として使用する画像
+
+function preload() {
+  // 画像を読み込む
+  fireworkImage = loadImage('surprised.png'); // 任意の画像のパス
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+  gravity = createVector(0, 0.15);
+  stroke(255);
+  strokeWeight(4);
+}
+
+function draw() {
+  background(0, 0, 0, 25);
+
+  if (random(1) < 0.03) {
+    fireworks.push(new Firework());
+  }
+
+  for (let i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+
+    if (fireworks[i].done()) {
+      fireworks.splice(i, 1);
+    }
+  }
+}
+
+class Firework {
+  constructor() {
+    this.firework = new Particle(random(width), height, true);
+    this.exploded = false;
+    this.particles = [];
+  }
+
+  done() {
+    return this.exploded && this.particles.length === 0;
+  }
+
+  update() {
+    if (!this.exploded) {
+      this.firework.applyForce(gravity);
+      this.firework.update();
+
+      if (this.firework.vel.y >= 0) {
+        this.exploded = true;
+        this.explode();
+      }
+    }
+
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].applyForce(gravity);
+      this.particles[i].update();
+
+      if (this.particles[i].done()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  }
+
+  explode() {
+    for (let i = 0; i < 100; i++) {
+      let p = new Particle(this.firework.pos.x, this.firework.pos.y, false);
+      this.particles.push(p);
+    }
+  }
+
+  show() {
+    if (!this.exploded) {
+      this.firework.show();
+    }
+
+    for (let i = 0; i < this.particles.length; i++) {
+      this.particles[i].show();
+    }
+  }
+}
+
+class Particle {
+  constructor(x, y, firework) {
+    this.pos = createVector(x, y);
+    this.firework = firework;
+    this.lifespan = 255;
+
+    if (this.firework) {
+      this.vel = createVector(0, random(-12, -8));
+    } else {
+      this.vel = p5.Vector.random2D();
+      this.vel.mult(random(2, 10));
+    }
+
+    this.acc = createVector(0, 0);
+    this.hu = random(255);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  update() {
+    if (!this.firework) {
+      this.vel.mult(0.9);
+      this.lifespan -= 4;
+    }
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  done() {
+    return this.lifespan < 0;
+  }
+
+  show() {
+    if (!this.firework) {
+      // Exploded particles (small dots)
+      strokeWeight(2);
+      stroke(this.hu, 255, 255, this.lifespan);
+      point(this.pos.x, this.pos.y);
+    } else {
+      // Firework (use image)
+      imageMode(CENTER);
+      image(fireworkImage, this.pos.x, this.pos.y, 30, 30); // 画像を30x30のサイズで表示
+    }
+  }
+}
